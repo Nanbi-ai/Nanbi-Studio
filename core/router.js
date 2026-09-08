c6a84b1ffa27593d4ff14feb23af7bc8d5bba5ea
Founder-Nanbi <rbms@nanbi.network>
2026-09-08 20:14:15 +0530

    Update router.js

--- core/router.js
+++ core/router.js
@@ -5,7 +5,7 @@ import { CryptoEngine } from './crypto_engine.js';
 // =======================================================================
 
 // Switch from Cloud to Localhost for SDLC
-const SUPABASE_URL = "http://127.0.0.1:54321";
+const SUPABASE_URL = "https://quality-pellet-gem.ngrok-free.dev";
 const SUPABASE_ANON_KEY = "sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH";
 window.nanbiDB = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
